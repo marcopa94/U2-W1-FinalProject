01690b1dda03598f1386c214f399d964d16832c2
Marco Pala <155745769+marcopa94@users.noreply.github.com>
2024-02-18 09:10:53 +0100

    reso smooth header- emme con intervallo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,18 @@
+//anim0 le m
+const letter = document.querySelector("#letterm").children;
+const numChildren = letter.length; // numero totale m
+setInterval(() => {
+  const randomNumber = Math.floor(Math.random() * numChildren);
+
+  //opaCizzo a turno velocita 7secondi
+  letter[randomNumber].style.opacity = letter[randomNumber].style.opacity === "1" ? "0" : "1";
+}, 7);
+
+//cambio colore header l'ho reso smooth aaggiugnendo transition nel foglio css
 const content = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 450) {
+  if (window.scrollY > 400) {
     content.style.backgroundColor = "white";
   } else {
     content.style.backgroundColor = "#ffc017";
