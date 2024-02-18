277d483c69404384eb46ec629b5a0d7fe2066996
Marco Pala <155745769+marcopa94@users.noreply.github.com>
2024-02-18 18:01:44 +0100

    piccole migliorie

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,22 @@
 //anim0 le m
 const letter = document.querySelector("#letterm").children;
-const numChildren = letter.length; // numero totale m
+const numC = letter.length; // numero totale m
 setInterval(() => {
-  const randomNumber = Math.floor(Math.random() * numChildren);
+  const randomNumber = Math.floor(Math.random() * numC);
 
   //opaCizzo a turno velocita 7secondi
   letter[randomNumber].style.opacity = letter[randomNumber].style.opacity === "1" ? "0" : "1";
-}, 7);
+}, 20);
 
 //cambio colore header l'ho reso smooth aaggiugnendo transition nel foglio css
 const content = document.querySelector("header");
-
+const bottone = document.querySelector(".btn1");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 400) {
     content.style.backgroundColor = "white";
+    bottone.style.backgroundColor = "#1A8917";
   } else {
     content.style.backgroundColor = "#ffc017";
+    bottone.style.backgroundColor = "black";
   }
 });
